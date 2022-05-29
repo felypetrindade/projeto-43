e7ea14dbed2b5039194ba332d263686b2b39064d
felypetrindade <97994964+felypetrindade@users.noreply.github.com>
2022-05-28 23:04:01 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 700, 170);
+
+  // Adicione código para exibir as outras duas perguntas.
+  fill('white');
+  textSize(15);
+  fill("lightblue");
+  text("Dica: De manha anda com 4 pernas, Ao meio dia com 2 pernas e ao anoitecer com 3 pernas", 100, 270);
+
+  fill('white');
+  textSize(15);
+  fill('lightblue');
+  text('Dica:  O que pode ser quebrada, mas nunca segurada?', 100,70);
+}
